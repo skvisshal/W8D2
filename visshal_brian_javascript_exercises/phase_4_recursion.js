c2a3a3d2331aa94083f5f61cb9746505593e9ef1
Visshal Suresh <skvisshal@gmail.com>
2021-11-23 16:25:03 -0500

    Phase 4 Completed

--- visshal_brian_javascript_exercises/phase_4_recursion.js
+++ visshal_brian_javascript_exercises/phase_4_recursion.js
@@ -70,3 +70,66 @@ function deepDup(arr) {
 }
 
 console.log(deepDup([1,2,3,4,5,[2,3]]));
+
+function bsearch(arr, target) {
+    if(arr.length === 0) {
+        return -1;
+    }
+
+    let midIdx = Math.floor(arr.length / 2);
+    if(target === arr[midIdx]) {
+        return midIdx;
+    } else if(target < arr[midIdx]) {
+        let left = arr.slice(0,midIdx);
+        return bsearch(left, target);
+    } else {
+        let right = arr.slice(midIdx+1, arr.length);
+        let result = bsearch(right, target);
+        if (result === -1) {
+            return -1;
+        } else {
+            return result + midIdx + 1
+        }
+    }
+}
+
+console.log(bsearch([-1,1,2,3, 4, 5, 6, 7, 9],0));
+
+function subsets(arr) {
+    if(arr.length === 0) {
+        return [[]]
+    }
+
+    let prev = subsets(arr.slice(0,arr.length-1))
+    let temp = prev.map(ele => ele.concat([arr[arr.length-1]]))
+    return prev.concat(temp)
+}
+
+console.log(subsets([1,2,3]))
+
+function merge(left, right) {
+    let mergedArr = [];
+
+    while(left.length > 0 && right.length > 0) {
+        if (left[0] < right[0]) {
+            mergedArr.push(left.shift());
+        } else {
+            mergedArr.push(right.shift());
+        }
+    }
+    mergedArr = mergedArr.concat(left)
+    mergedArr = mergedArr.concat(right)
+    return mergedArr;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let midIdx = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, midIdx));
+    let right = mergeSort(arr.slice(midIdx));
+    return merge(left,right);
+}
+
+console.log(mergeSort([7,5,9,4,2,1,3,0]))
